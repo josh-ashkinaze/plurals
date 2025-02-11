f347996ffc7020059ee055f2c28fb45beb1da4eb
josh-ashkinaze <jashkina@umich.edu>
2025-02-11 11:14:05 -0500

    (release)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read_requirements():
 
 setup(
     name="plurals",
-    version="0.2.30",
+    version="0.2.31",
     description="A package for simulated social ensembles.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
