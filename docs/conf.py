eb4d442f52bcf195d2a31b4ad205c70f805c8b8e
josh-ashkinaze <josh.ashkinaze@gmail.com>
2025-12-22 11:28:34 -0500

    added best-of-n

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,11 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    "show_navbar_depth": 10,  # Controls depth of expanded items (default is 1)
+    "collapse_navbar": False,  # Keep navbar expandable (this is the default)
+}
+
 # html_sidebars = {
 #    '**': ['sidebar.html', 'sourcelink.html', 'searchbox.html']
 # }
