ac2bdf087cf349a009e7bd6a6ac80e53d4f71932
josh-ashkinaze <josh.ashkinaze@gmail.com>
2026-02-22 05:04:16 -0500

    push to pypi

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read_requirements():
 
 setup(
     name="plurals",
-    version="0.3",
+    version="0.4",
     description="A package for simulated social ensembles.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
