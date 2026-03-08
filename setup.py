49435d74a3e3bff43c8f2f71edb68e6d44ecb831
josh-ashkinaze <josh.ashkinaze@gmail.com>
2026-03-08 10:04:30 -0400

    pypi release

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 def read_requirements():
     with open('requirements.txt') as req_file:
         return req_file.read().splitlines()
-
 setup(
     name="plurals",
     version="0.5",
